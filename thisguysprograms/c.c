51ee3181c51d329c79f36652360595fab3f86095
Andrés Reyes <andresreyesev@gmail.com>
2019-03-13 17:14:22 -0600

    This guy's programs complete

--- thisguysprograms/c.c
+++ thisguysprograms/c.c
@@ -3,12 +3,12 @@
 #include <time.h>
 
 /*
-38.- 10. Escribe un programa que reciba un número entero N del usuario e imprima el contorno de un
-cuadrado de N x N de asteriscos. Ej.
+48. Escribe un programa que reciba un número entero N del usuario e imprima el contorno de
+pirámide de N pisos de asteriscos. Ej.
 >> 3
-***
-* *
-***
+      *
+     * *
+    *****
 */
 
 //Pide un número al usuario y lo regresa
@@ -21,25 +21,39 @@ int obtener_dato() {
     return x;
 }
 
-void dibujar_cuadrado(int x) {
+//Verifica que el número sea positivo, regresa 0 si lo es, 1 si es no-positivo
+int verificar_valor (int x) {
+    int valido = 1;
+
+    if (x <= 0)
+        printf ("El numero debe de ser mayor que 0\n");
+    else
+        valido = 0;
+
+    return valido; 
+}
+
+void imprimir_rombo (int x) {
     for (int i = 0; i < x; i++) {
-        if (i == 0 || i == x - 1)
-            for (int j = 0; j < x; j++) 
-                printf("*");
-        else
-            for (int j = 0; j < x; j++) 
-                if (j == 0 || j == x - 1)
-                    printf("*");
-                else
-                    printf(" ");
+        for (int j = x - i - 1; j >= 0; j--)
+            printf(" ");
+
+        for (int j = 0; j < i * 2 + 1; j++)
+            printf("*");
+        
         printf("\n");
     }
 }
 
 int main (void) {
-    int x = obtener_dato();
+    int x, valido;
+    
+    do {
+        x = obtener_dato();
+        valido = verificar_valor(x);
+    } while (valido != 0);
 
-    dibujar_cuadrado(x);
+    imprimir_rombo(x);
 
     return 0;
 }
